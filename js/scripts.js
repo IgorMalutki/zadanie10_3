ca22acb843f20c42a4aabbb58b9a80f30a6dd52f
IgorMalutki <ireneuszzadlo@gmail.com>
2017-10-25 10:39:55 +0200

    zadanie10_3_karuzela

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,51 @@
 ﻿// scripts.js
 
+//  zadanie 10_3
+
 var carouselList = $("#carousel ul");
 
 
 $(function(){
-	setInterval(changeSlide, 3000); 
+	carouselList.css({marginLeft:-400});
+//	setInterval(changeSlide, 3000); 
 });
 
+function changeSlideLeft() {
+	carouselList.animate({'marginLeft':-800}, 500, moveFirstSlide);	
+}
 
-
-
-function changeSlide() {
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);	
+function changeSlideRight() {
+	carouselList.animate({'marginLeft': 0}, 500, moveLastSlide);	
 }
 
 function moveFirstSlide() {
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
 	lastItem.after(firstItem);	
-	carouselList.css({marginLeft:0});
+	carouselList.css({marginLeft:-400});
 }
+
+function moveLastSlide() {
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	firstItem.before(lastItem);	
+	carouselList.css({marginLeft:-400});
+}
+
+$('i.fa-chevron-left').click(function() {
+changeSlideLeft();
+console.log("ok-left");
+});
+
+$('i.fa-chevron-right').click(function() {
+changeSlideRight();
+console.log("ok-right");
+});
+
+
+
+
+
 
 /*var paragraphs = $('p');
 paragraphs.each(function(index, element) {
